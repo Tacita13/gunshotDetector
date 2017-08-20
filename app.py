1ebf1bdd430cee1db065ee7ee1409f90e675c8a1
Tacita13 <isamar.lo15425@gmail.com>
2017-08-19 21:57:21 -0700

    Add json.serial for timestamp, add json.dumps to format JSON

--- app.py
+++ app.py
@@ -1,19 +1,23 @@
 from flask import Flask, render_template, jsonify
 import db_output
+import json
+from datetime import date, datetime
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-        return render_template('index.html')
+def json_serial(obj):
 
-@app.route('/dashboard')
+        if isinstance(obj, (datetime,date)):
+                return obj.isoformat();
+
+@app.route('/')
 def dashboard():
         d = db_output.db.query(db_output.latest_event)
         d[0]["longitude"] = str(d[0]["longitude"])
         d[0]["latitude"] = str(d[0]["latitude"])
         d[0]["soundlevel"] = str(d[0]["soundlevel"])
-        return jsonify(d[0])
+        res = json.dumps(d[0], default=json_serial)
+        return render_template('index.html', summary=res)
 
 if __name__=='__main__':
         app.run(debug=True,  host='0.0.0.0', port=5000 )
